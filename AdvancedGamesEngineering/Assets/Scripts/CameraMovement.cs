c53d7c740971e01aa3cbb43f1244105a9bbe9392
Olav Jakobson Digranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-10-12 17:37:10 +0100

    Camera movement

---
+++ AdvancedGamesEngineering/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    //https://dev.to/matthewodle/simple-3d-camera-movement-1lcj
+    private float moveSpeed = 0.05f;
+    private float scrollSpeed = 5f;
+
+    void Update () {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) {
+            transform.position += moveSpeed * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0) {
+            transform.position += scrollSpeed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
+        }
+    }
+}
