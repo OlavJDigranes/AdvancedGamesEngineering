0b002269cf20d5547e33f6491252114245acca54
OlavJDigranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-11-09 10:33:22 +0000

    Update MainMenuManager.cs
    Updated input fields

--- AdvancedGamesEngineering/Assets/Scripts/MainMenuManager.cs
+++ AdvancedGamesEngineering/Assets/Scripts/MainMenuManager.cs
@@ -7,15 +7,15 @@ public class MainMenuManager : MonoBehaviour
 {
     //Take in the customising parameters and use them to generate the planetary system. 
     public static int numOfPlanets; 
-    public static int starMass;
-    public static int starLuminocity;
-    public static int starAge; 
+    public static string spectralClassification;
+    //public static int starLuminocity;
+    public static int absoluteMagnitude; 
 
     public GameObject menuPanel;
 
-    private string inputStarMass; 
-    private string inputStarAge; 
-    private string inputStarLuminocity; 
+    private string inputAbsoluteMagnitude; 
+    private string inputSpectralClassification; 
+    //private string inputStarLuminocity; 
     private string inputNumOfPlanets; 
 
     // Start is called before the first frame update
@@ -32,15 +32,21 @@ public class MainMenuManager : MonoBehaviour
 
     // This function starts the simulatin using the gathered parameters. 
     public void StartSim(){
+        /*
         if(int.TryParse(inputStarMass, out int mass)){
             starMass = mass; 
         } 
         if(int.TryParse(inputStarAge, out int age)){
             starAge = age; 
         } 
-        if(int.TryParse(inputStarLuminocity, out int lumin)){
-            starLuminocity = lumin; 
-        } 
+        */
+        //if(int.TryParse(inputStarLuminocity, out int lumin)){
+        //    starLuminocity = lumin; 
+        //} 
+        spectralClassification = inputSpectralClassification;
+        if(int.TryParse(inputAbsoluteMagnitude, out int mv)){
+            absoluteMagnitude = mv; 
+        }
         if(int.TryParse(inputNumOfPlanets, out int nop)){
             numOfPlanets = nop; 
         } 
@@ -48,20 +54,22 @@ public class MainMenuManager : MonoBehaviour
     }
 
     //These functions are used for the InputFields in the main menu scene. Debug Logs are due to an editor bug as of 08.10.2022
-    public void ReadStringinputStarMass(string s){
-        inputStarMass = s;
+    public void ReadStringinputSpectralClassification(string s){
+        inputSpectralClassification = s;
         Debug.Log(s); 
     }
 
-    public void ReadStringinputStarAge(string s){
-        inputStarAge = s;
+    public void ReadStringinputAbsoluteMagnitude(string s){
+        inputAbsoluteMagnitude = s;
         Debug.Log(s); 
     }
 
+    /*
     public void ReadStringinputStarLuminocity(string s){
         inputStarLuminocity = s;
         Debug.Log(s); 
     }
+    */
 
     public void ReadStringinputNumOfPlanets(string s){
         inputNumOfPlanets = s;
