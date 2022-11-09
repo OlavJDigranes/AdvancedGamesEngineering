0aa3be79a2dac2886acc95e6f8998785a9a97edb
OlavJDigranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-11-09 10:33:47 +0000

    Update Star.cs
    Star now follows Hertzsprung-Russell and Harvard

--- AdvancedGamesEngineering/Assets/Scripts/Star.cs
+++ AdvancedGamesEngineering/Assets/Scripts/Star.cs
@@ -1,3 +1,4 @@
+using System; 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,13 @@ using UnityEngine;
 public class Star : MonoBehaviour
 {
     //Variables
-    int mass;
-    int luminocity;
-    int age;
+    public double mass;
+    public double luminocity;
+    public double age;
+    public double radius; 
+    public string spectralClassification;
+    public int absoluteMagnitude;
+    public Color colour;  
 
     public GameObject star;
     public GameObject menu; 
@@ -18,7 +23,6 @@ public class Star : MonoBehaviour
 
     //set up some varaiables
     void Awake() {
-        FIllColours(); 
         mainMenuMngr = menu.GetComponent<MainMenuManager>(); 
     }
 
@@ -29,79 +33,87 @@ public class Star : MonoBehaviour
         star.transform.position = new Vector3(0, 0, 0);
 
         //Take in variables from menu
-        mass = MainMenuManager.starMass;
-        luminocity = MainMenuManager.starLuminocity;
-        age = MainMenuManager.starAge;
+        spectralClassification = MainMenuManager.spectralClassification; 
+        absoluteMagnitude = MainMenuManager.absoluteMagnitude; 
 
         Debug.Log("Sim Loaded");
 
         //Funcitons to generate star
-        DetermineColour(age);
-        DetermineLightEmission(luminocity);
-        DetermineSizeAndPull(mass);
+        GenerateStar(absoluteMagnitude, spectralClassification);
    
     }
 
-    void DetermineColour(int a){
-        /*
-        The age of the star determines the colour. This is a simple early implementation for the sake of proving the concept. 
-        */
-        var sphereRenderer = star.GetComponent<Renderer>();
-
-        //Reverse and rotate the colour wheel as younger stars are more blue. A more accurate representation of star colour requires implementation of Hertzsprung-Russell values. 
-        float rotatedAge = 360f - (float)a; 
-        float ageHue = rotatedAge/360f; 
-
-        Debug.Log(Color.HSVToRGB(ageHue, 1f, 1f));
-        //Debug.Log(Color.HSVToRGB(a, Random.Range(0f, 1f), Random.Range(0f, 1f)));
-
-        sphereRenderer.material.SetColor("_DetailColor", Color.white);
-        sphereRenderer.material.SetColor("_StarColor", Color.HSVToRGB(ageHue, 1f, 1f));
-    }
-
-    void DetermineLightEmission(int l){
-        //Turn the star into the light in the scene. Use colour of star to colour the emitted light. 
-
-        //Turn alpha to a float between 0 and 1
-        float alpha = (float)l/10.0f;
-
-        //Colour handling 
-        var sphereRenderer = star.GetComponent<Renderer>(); 
-        Color tempColour; 
-        tempColour = sphereRenderer.material.GetColor("_StarColor"); 
-        tempColour.a = alpha; 
-        sphereRenderer.material.SetColor("_StarColor", tempColour); 
-    }
-
-    void DetermineSizeAndPull(int m){
-        //Directly set rigidbody mass.  
+    void GenerateStar(int mv, string type){
+        //Temp Variables
+        Color tempCol; 
+        float colourAlpha; 
+        int mvMoved;
+        int mvRev;
+        float randMass = 0.0f; 
+        float randScale = 0.0f; 
+        float randKelvin = 0.0f;
+        double scalarMass = 2*Math.Pow(10.0, 30.0); //kg
+        double scalarScale = 695700.0; //km
         rb.GetComponent<Rigidbody>();
-        rb.mass = (m); 
+
+        if(type == "O"){
+            randMass = (UnityEngine.Random.Range(16.1f, 150f)); 
+            randScale = UnityEngine.Random.Range(6.6f, 1500.0f);
+            randKelvin = UnityEngine.Random.Range(30000.0f, 40000.0f);
+        }
+        if(type == "B"){
+            randMass = (UnityEngine.Random.Range(2.1f, 16f)); 
+            randScale = UnityEngine.Random.Range(1.8f, 6.6f);
+            randKelvin = UnityEngine.Random.Range(10000.0f, 30000.0f);
+        }
+        if(type == "A"){
+            randMass = (UnityEngine.Random.Range(1.4f, 2.1f)); 
+            randScale = UnityEngine.Random.Range(1.4f, 1.8f);
+            randKelvin = UnityEngine.Random.Range(7500.0f, 10000.0f);
+        }
+        if(type == "F"){
+            randMass = (UnityEngine.Random.Range(1.04f, 1.4f)); 
+            randScale = UnityEngine.Random.Range(1.15f, 1.4f);
+            randKelvin = UnityEngine.Random.Range(6000.0f, 7500.0f);
+        }
+        if(type == "G"){
+            randMass = (UnityEngine.Random.Range(0.8f, 1.04f)); 
+            randScale = UnityEngine.Random.Range(0.96f, 1.15f);
+            randKelvin = UnityEngine.Random.Range(5200.0f, 6000.0f);
+        }
+        if(type == "K"){
+            randMass = (UnityEngine.Random.Range(0.45f, 0.8f));
+            randScale = UnityEngine.Random.Range(0.7f, 0.96f); 
+            randKelvin = UnityEngine.Random.Range(3700.0f, 5200.0f);
+        }
+        if(type == "M"){
+            randMass = (UnityEngine.Random.Range(0.08f, 0.45f)); 
+            randScale = UnityEngine.Random.Range(0.1f, 0.7f);
+            randKelvin = UnityEngine.Random.Range(2400.0f, 3700.0f);
+        }
+        
+        //set mass
+        mass = (double)randMass * scalarMass; 
+        rb.mass = (float)mass; 
+
         var scale = star.transform.localScale; 
 
-        //Set scale based on mass
-        scale *= (m); 
+        //Set scale
+        radius = (double)randScale * scalarScale; 
+        scale *= (float)radius * 2.0f; 
         star.transform.localScale = scale; 
-    }
 
-    void FIllColours(){
-        //This functions fills the colour array in a gradient from red to blue. 
-        colours = new Color[12]; 
-        colours[0] = Color.white; 
-        colours[1] = Color.red;
-        colours[2] = new Color(0.8f, 0.2f, 0.0f, 1.0f); 
-        colours[3] = new Color(0.6f, 0.4f, 0.0f, 1.0f); 
-        colours[4] = new Color(0.4f, 0.6f, 0.0f, 1.0f);
-        colours[5] = new Color(0.2f, 0.8f, 0.0f, 1.0f);
-        colours[6] = new Color(0.0f, 1.0f, 0.0f, 1.0f);
-        colours[7] = new Color(0.0f, 0.8f, 0.2f, 1.0f);
-        colours[8] = new Color(0.0f, 0.6f, 0.4f, 1.0f);
-        colours[9] = new Color(0.0f, 0.4f, 0.6f, 1.0f);
-        colours[10] = new Color(0.0f, 0.2f, 0.8f, 1.0f);
-        colours[11] = Color.blue; 
-    }
-
-    
+        //Colour 
+        tempCol = Mathf.CorrelatedColorTemperatureToRGB(randKelvin);
+        mvMoved = mv + 10;
+        mvRev = 30 - mvMoved; 
+        colourAlpha = (float)mvRev/30;
+        tempCol.a = colourAlpha; 
+        colour = tempCol; 
+        var sphereRenderer = star.GetComponent<Renderer>();
+        sphereRenderer.material.SetColor("_DetailColor", Color.white);
+        sphereRenderer.material.SetColor("_StarColor", tempCol);
+    }   
 }
 
 /*NOTES:
